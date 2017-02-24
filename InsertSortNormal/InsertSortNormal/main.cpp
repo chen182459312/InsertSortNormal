074aaedad6d4931e2f69ed2e91e54f37a1fa5bd2
chentao <chentao@stonedeiMac.local>
2017-02-24 10:11:14 +0800

    delete redundancy code

--- InsertSortNormal/InsertSortNormal/main.cpp
+++ InsertSortNormal/InsertSortNormal/main.cpp
@@ -10,14 +10,9 @@
 #include "InsertSortNormal.h"
 using namespace std;
 
-
 const int bufferSize = 5;
 
-void printArray(int array[], int count);
-
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    
     int array[bufferSize] = {0};
     for(int index = 0; index < bufferSize; index++) {
         array[index] = arc4random()%20;
